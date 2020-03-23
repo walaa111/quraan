aced446ec26965133cb75b64b39c4ff0ac7c9d7e
walaa111 <walaagamil18@gmail.com>
2020-03-23 13:30:15 +0200

    add

--- quraan/www/js/index.js
+++ quraan/www/js/index.js
@@ -93,23 +93,7 @@ var app = {
        //admob
 
 
-       //onesignal
-       // Add to index.js or the first page that loads with your app.
-// For Intel XDK and please add this to your app.js.
 
-document.addEventListener('deviceready', function () {
-  // Enable to debug issues.
-  window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
-  alert("test")
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
-
-  window.plugins.OneSignal
-    .startInit("1efd3206-9517-4006-b656-8fd0e9465981")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
-}, false);
     },
 
 
