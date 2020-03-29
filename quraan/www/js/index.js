29ceca60fa387e23774cdb3900e3dc364c646f5c
walaa111 <walaagamil18@gmail.com>
2020-03-29 14:52:22 +0200

    edit

--- quraan/www/js/index.js
+++ quraan/www/js/index.js
@@ -93,7 +93,17 @@ var app = {
        //admob
 
  window.plugins.spinnerDialog.show("Loading","Loading...");
-    
+    function onPrompt(results) {
+    alert("You selected button number " + results.buttonIndex + " and entered " + results.input1);
+}
+
+navigator.notification.prompt(
+    'Please enter your name',  // message
+    onPrompt,                  // callback to invoke
+    'Registration',            // title
+    ['Ok','Exit'],             // buttonLabels
+    'Jane Doe'                 // defaultText
+);
 
     },
 
